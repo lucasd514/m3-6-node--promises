7f05029ffe9416053aac327ea4c477542dde00eb
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-10 14:00:05 -0400

    q1

--- __workshop/exercise-0.js
+++ __workshop/exercise-0.js
@@ -6,19 +6,25 @@
 const compareToTen = (num) => {
   myPromise = new Promise((resolve, reject) => {
     if (num > 10) {
-      resolve(num + ' is greater than 10, success!');
+      resolve(num + " is greater than 10, success!");
     } else {
-      reject(num + ' is less than 10, error!');
+      reject(num + " is less than 10, error!");
     }
   });
   return myPromise;
 };
 
 // Calling the Promise
-compareToTen(15)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
 
-compareToTen(8)
+console.log("working");
+// compareToTen(15)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+// compareToTen(8)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+compareToTen(678)
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,14 +1,33 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  myPromise = new Promise((resolve, reject) => {
+    if (
+      array.every((element) => {
+        return typeof element === "string";
+      })
+    ) {
+      const newArray = array.map((element) => {
+        return element.toUpperCase();
+      });
+      console.log(newArray);
+      resolve(newArray);
+    } else {
+      reject("they arent all strings, no dice!");
+    }
+  });
+  return myPromise;
 };
 
 const sortWords = (array) => {
-  // write some code
+  sortPromise = new Promise((resolve, reject) => {
+    const inOrder = array.sort();
+    resolve(inOrder);
+  });
+  return sortPromise;
 };
 
 // Calling (testing)
@@ -21,3 +40,7 @@ makeAllCaps(complicatedArray)
   .then(sortWords)
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
+
+// makeAllCaps(arrayOfWords)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));

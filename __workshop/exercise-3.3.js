0c76376df878dce53a94f912b678d630201969f4
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-10 18:05:12 -0400

    done to min

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -1,18 +1,25 @@
 // Exercise 3.3 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require("opencage-api-client");
+require("dotenv").config();
 
 function getAddressFromPosition(lat, lng) {
+  const test1 = lat + ", " + lng;
+  console.log(test1);
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: "a55df9ce8238421bb0b6d4b2125fa4b8",
+    q: test1,
   };
-
-  // return ...
+  return opencage
+    .geocode(requestObj)
+    .then((response) => response.results[0])
+    .catch((error) => {
+      console.log("error", error.message);
+    });
 }
+// return ...
 
-getAddressFromPosition('48.8584', '2.2945').then((response) =>
+getAddressFromPosition("45.5580", "73.5519").then((response) =>
   console.log(response)
 );

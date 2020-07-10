29e1a348f9f2ece70aaee1e19622514bd112d80c
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-10 15:56:04 -0400

    2.3

--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -2,6 +2,18 @@
 // ----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require("request-promise");
 
+const greeting = (langCode) => {
+  return request("https://journeyedu.herokuapp.com/hello/" + langCode) // 1
+    .then((response) => JSON.parse(response))
+    .then((parsedResponse) => {
+      return parsedResponse.data.text; // 2
+    })
+    .catch((err) => {
+      return err.error ? JSON.parse(err.error) : err;
+    });
+};
 // get the code you wrote in 2.2 and paste it here...
+
+greeting("yomama").then((result) => console.log(result)); // { lang: "French", code: "FR", text: "Bonjour" }
